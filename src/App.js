49170cd9b05e19f166bfdab0217c9a88c17d8e03
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-09-28 21:27:34 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Products from "./components/Products";
 
-// Limitations of the app:
-// 1. No error handling for failed API requests.
+// Limitations of the web:
+// 1. No input validation for user-searched data(ex: if a product is not there, no msg is printed).
 // 2. No detailed product view or additional product information.
 // 3. Limited responsiveness for different screen sizes.
 // 4. No user authentication or authorization.
